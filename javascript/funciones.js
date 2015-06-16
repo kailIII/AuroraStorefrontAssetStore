a784e16df16d83b8f657068e1c954ab80b0a2754
Selene <abuelo40@gmail.com>
2015-06-16 11:06:25 -0500

    menu
    despliegue entre escritorio y móvil y viceversa.  Trabajo menú de un
    solo nivel

--- javascript/funciones.js
+++ javascript/funciones.js
@@ -1,8 +1,15 @@
 /*Script telmexianos*/
 
+
+/*Función para checar el ancho de la pantalla.  Incorrecto poner $(window).width ya que difiere del usado por media queries*/
 function checkWidth(){
-	var windowSize = $(window).width();
-	return  windowSize;
+		var e = window, a = 'inner';
+		if(!('innerWidth' in window)){
+			a = 'client';
+			e = document.documentElement || document.body;
+		}
+		var widthD = e[a+'Width'];
+		return widthD		
 }
 
 checkWidth();
@@ -27,17 +34,18 @@ $( document ).ready(function() {
 
 
 
-
+/*Función hover menú escritorio*/
 function menuF(){
 	
-	if ($(window).width() > 952) {
+	if (checkWidth() > 952) {
 	var claseMenu = "";
 	//console.log("mayor a 952");
 		$(".menuLink").hover(
 			function () {
-				
-				claseMenu = $(this).attr('rel');			
-				$(this).find('div').show();
+				if($(this).find('li').length > 0){
+					claseMenu = $(this).attr('rel');			
+					$(this).find('div').show();
+				}
 			}, 
 			function () {
 				claseMenu = $(this).attr('rel');
@@ -48,7 +56,9 @@ function menuF(){
 
 	}
 	else {
-
+		
+	   $('.menuLink').unbind('mouseenter mouseleave');
+	   
 	   console.log('Less than 960');
 	}
 }
@@ -58,11 +68,19 @@ menuF();
 var responsiveMenu = function(){
 	var activeButton= $('#departmentsButton');
 	var menuCont =$('#departmentsMenu');
+	var ibmBtn = menuCont.find('span[role="presentation"]');
 	activeButton.unbind('click');
 	
+	ibmBtn.closest('a').hide();
+	
 	if(checkWidth() < 953){
+		
 		menuCont.css('display', 'none');
 		activeButton.removeClass('selected');
+		
+		var isActive = $('.menuCategorias').find('active').length;
+
+		
 		activeButton.click(function(e){
 			e.stopPropagation()
 			menuCont.animate({width: 'toggle'});
@@ -77,27 +95,38 @@ var responsiveMenu = function(){
 		elemClick.unbind('click');
 		var returnBtn = $('<div class="menuReturn"></div>')
 		elemClick.click(function(e){
-			e.preventDefault();
+			
 			e.stopPropagation();
 			var siblingsElem = $(this).parent('ul').find('>li');
 			var ulParent = $(this).find('ul')
 			var childUl = $(this).find('ul').eq(0);
 			var textParent=$(this).find('.menuLink').eq(0).text();
+			var divParent = childUl.closest('div');
+			
+			if(divParent.css('display')== "none"){
+				divParent.show();
+			}
 			
 			
 			if(ulParent.length > 1){
+				e.preventDefault();
 				siblingsElem.not(this).hide();
 				childUl.show();
 				childUl.find('li').show();
 				childUl.find('ul').hide();
 				returnBtn.text('Regresar a Menu principal' );
 			}else{
-				siblingsElem.not(this).each(function(){
-					$(this).find('ul').slideUp();
-				})
-				childUl.slideToggle();
+				if(ulParent.find('li').length > 0){
+					siblingsElem.not(this).each(function(){
+						$(this).find('ul').slideUp();
+					})
+					childUl.slideToggle();
+					e.preventDefault();
+				}else{
+					
+				}
 			}
-			$(this).parent('#departmentsMenu').prepend(returnBtn);
+			//$(this).parent('#departmentsMenu').prepend(returnBtn);
 		});
 		
 		returnBtn.click(function(){
@@ -117,9 +146,15 @@ var responsiveMenu = function(){
 		
 	}else{
 		menuCont.show();
-		var elemFirst = menuCont.find('li');
+		var elemFirst = menuCont.find('>li');
+		var allElems = elemFirst.find('li');
+		var listMen = menuCont.find('ul').show();
 		elemFirst.each(function(){
 			$(this).css('display','inline-block');
+		})
+		
+		allElems.each(function(){
+			$(this).show();
 		})
 		menuCont.find('li').unbind('click');
 		$('.menuReturn').remove();
