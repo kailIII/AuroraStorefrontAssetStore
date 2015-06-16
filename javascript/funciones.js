df824532af26539a1b0d02c0ab0772b6be2e6f6b
Selene <abuelo40@gmail.com>
2015-06-15 21:32:58 -0500

    menu
    responsive

--- javascript/funciones.js
+++ javascript/funciones.js
@@ -1,3 +1,18 @@
+/*Script telmexianos*/
+
+function checkWidth(){
+	var windowSize = $(window).width();
+	return  windowSize;
+}
+
+checkWidth();
+
+/*Resize window*/
+$(window).resize(function(){
+		responsiveMenu();
+		menuF();
+})
+
 
 $( document ).ready(function() {
 
@@ -6,7 +21,8 @@ $( document ).ready(function() {
 	    $(".asistenciaLista").toggle();
 	});
 
-
+	responsiveMenu();
+	
 });
 
 
@@ -14,7 +30,7 @@ $( document ).ready(function() {
 
 function menuF(){
 	
-	if ($(window).width() > 952) {
+	if ($(window).width() >= 952) {
 	var claseMenu = "";
 	console.log("mayor a 952");
 		$(".menuLink").hover(
@@ -28,12 +44,38 @@ function menuF(){
 			   $('div.'+claseMenu).hide(1);
 			}
 		);
-		
-
 	}
 	else {
-	   console.log('Less than 960');   
+	   console.log('Less than 960');
 	}
 }
 
 menuF();
+
+var responsiveMenu = function(){
+	
+	
+	var activeButton= $('#departmentsButton');
+	var menuCont =$('#departmentsMenu');
+	activeButton.unbind('click');
+	
+	if(checkWidth() < 953){
+		menuCont.css('display', 'none');
+		activeButton.click(function(e){
+			e.stopPropagation()
+			menuCont.animate({width: 'toggle'});
+			/*if(menuCont.hasClass('open')== false){
+			   menuCont.stop().animate({width: 'toggle'},1000,function(){$(this).addClass('open')});
+			}else{
+			menuCont.stop().animate({width: '0px'},1000,function(){
+				$(this).removeClass('open')
+			});
+			}*/
+		})
+	}else{
+		menuCont.css('display', 'block');
+	}
+	
+
+}
+
