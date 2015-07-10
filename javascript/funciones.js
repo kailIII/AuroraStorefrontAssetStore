86a722d677eada2573edfeb0cd7d5cc503e0ec54
Alex <abuelo40@gmail.com>
2015-07-09 20:33:26 -0500

    Ficha producto CAMBIOS ESTILOS

--- javascript/funciones.js
+++ javascript/funciones.js
@@ -1,4 +1,17 @@
 /*Script telmexianos /Pixseles*/
+// Internet Explorer
+window.onload = function()
+{
+     document.onselectstart = function()
+     {
+          return false;
+     } 
+// Firefox
+     document.onmousedown = function()
+     {
+          return false;
+     }
+}
 	
 	function registroUsuario(){
 		
